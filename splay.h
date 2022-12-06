9dc5f21f14d8965e51f65d2f8d163d6e2e2fc157
allenvliu <98777272+allenvliu@users.noreply.github.com>
2022-12-05 22:55:32 -0500

    Update splay.h

--- splay.h
+++ splay.h
@@ -1,1 +1,45 @@
-as
+#ifndef __SPLAY_H__
+#define __SPLAY_H__
+
+#include <iostream>
+#include <sstream>
+
+class Node {
+    
+    private:
+    std::string key;
+    Node* left;
+    Node* right;
+    
+    public:
+    Node(std::string key)
+    ~Node();
+    
+    friend class splayTree;
+};
+
+class splayTree {
+    
+    public:
+    
+        splayTree();
+        ~splayTree();
+        
+        void insert(std::string)
+        int height();
+
+        bool search(std::string key)
+    
+    private:
+    
+        Node* root;
+        
+        Node* insert(std::string key, Node* root);
+        
+        int height(Node* root)
+        void preorder(Node* root, std::cout)
+        void delete(Node* key);
+        bool search(std::string key, Node* root);
+};
+
+#endif
